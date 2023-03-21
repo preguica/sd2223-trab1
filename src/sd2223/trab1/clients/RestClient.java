5f4cb18b4809e36c5acf2d1e4a854b65e9a5c629
barbara-correia <barbaracc2010@gmail.com>
2023-03-21 19:27:44 +0000

    🔨 Added communication error handling - content from aula3

---
+++ src/sd2223/trab1/clients/RestClient.java
@@ -0,0 +1,59 @@
+package sd2223.trab1.clients;
+
+import java.net.URI;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+
+import jakarta.ws.rs.ProcessingException;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+
+public class RestClient {
+    private static Logger Log = Logger.getLogger(RestClient.class.getName());
+
+    protected static final int READ_TIMEOUT = 5000;
+    protected static final int CONNECT_TIMEOUT = 5000;
+
+    protected static final int RETRY_SLEEP = 3000;
+    protected static final int MAX_RETRIES = 10;
+
+    final URI serverURI;
+    final Client client;
+    final ClientConfig config;
+
+    RestClient(URI serverURI) {
+        this.serverURI = serverURI;
+        this.config = new ClientConfig();
+
+        config.property(ClientProperties.READ_TIMEOUT, READ_TIMEOUT);
+        config.property(ClientProperties.CONNECT_TIMEOUT, CONNECT_TIMEOUT);
+
+        this.client = ClientBuilder.newClient(config);
+    }
+
+    protected <T> T reTry(Supplier<T> func) {
+        for (int i = 0; i < MAX_RETRIES; i++)
+            try {
+                return func.get();
+            } catch (ProcessingException x) {
+                System.err.println(x.getMessage());
+                Log.fine("ProcessingException: " + x.getMessage());
+                sleep(RETRY_SLEEP);
+            } catch (Exception x) {
+                Log.fine("Exception: " + x.getMessage());
+                x.printStackTrace();
+                break;
+            }
+        return null;
+    }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException x) { // nothing to do...
+        }
+    }
+}
